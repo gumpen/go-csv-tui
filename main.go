fef99287d45a1d23f6e5d6d5a81a2e4ff2924d83
gumpen <shuhei1993727@gmail.com>
2019-02-09 15:25:00 +0900

    Output input string when pushed 'enter' key

--- main.go
+++ main.go
@@ -10,6 +10,7 @@ const coldef = termbox.ColorDefault
 func drawBox() {
 	termbox.Clear(coldef, coldef)
 
+	// インプット部分の作成
 	var attr termbox.Attribute
 	var cells []termbox.Cell
 	for _, c := range inputString {
@@ -21,6 +22,20 @@ func drawBox() {
 		})
 	}
 
+	// アウトプット部分の作成
+	var outputCells [][]termbox.Cell
+	for i, row := range outputRows {
+		outputCells = append(outputCells, []termbox.Cell{})
+		for _, c := range row {
+			outputCells[i] = append(outputCells[i], termbox.Cell{
+				Ch: c,
+				Fg: attr,
+				Bg: termbox.ColorDefault,
+			})
+		}
+	}
+
+	// インプット部分の書き込み
 	i := 0
 	for _, c := range cells {
 		termbox.SetCell(0+i, 0, c.Ch, c.Fg, c.Bg)
@@ -32,12 +47,27 @@ func drawBox() {
 		i += w
 	}
 
+	w := 0
+	for i, row := range outputCells {
+		for _, c := range row {
+			termbox.SetCell(0+w, 1+i, c.Ch, c.Fg, c.Bg)
+			width := runewidth.RuneWidth(c.Ch)
+			if width == 0 || width == 2 && runewidth.IsAmbiguousWidth(c.Ch) {
+				width = 1
+			}
+			w += width
+		}
+		w = 0
+	}
+
+	// カーソルの書き込み
 	termbox.SetCursor(RuneLength(inputString), 0)
 
 	termbox.Flush()
 }
 
 var inputString []rune
+var outputRows []string
 
 func main() {
 	err := termbox.Init()
@@ -55,6 +85,8 @@ MAINLOOP:
 			switch ev.Key {
 			case 0:
 				inputChar(ev.Ch)
+			case termbox.KeyEnter:
+				addRow(inputString)
 			case termbox.KeyEsc:
 				break MAINLOOP
 			}
@@ -78,4 +110,8 @@ func RuneLength(str []rune) int {
 		l += w
 	}
 	return l
+}
+
+func addRow(runes []rune) {
+	outputRows = append(outputRows, string(runes))
 }
